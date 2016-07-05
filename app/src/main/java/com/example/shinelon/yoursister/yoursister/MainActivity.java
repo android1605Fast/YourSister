94cc17de1ea5b10ecb1bdd5133f395e6b165827e
hanxiaosong <635219750@qq.com>
2016-07-05 20:31:29 +0800

    searchlayout

---
+++ app/src/main/java/com/example/shinelon/yoursister/yoursister/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.shinelon.yoursister.yoursister;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.qf.administrator.yoursister.R;
+import com.qf.administrator.yoursister.SearchActivity;
+import com.qf.administrator.yoursister.fragment.PagerFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Adapter.ViewPagerAdapter;
+
+public class MainActivity extends AppCompatActivity{
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private List<PagerFragment> fragments;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+        initView();
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), fragments);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void initView(){
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+    }
+
+    private void initData(){
+        fragments = new ArrayList<>();
+        fragments.add(new PagerFragment());
+        fragments.add(new PagerFragment());
+        fragments.add(new PagerFragment());
+        fragments.add(new PagerFragment());
+        fragments.add(new PagerFragment());
+        fragments.add(new PagerFragment());
+        fragments.add(new PagerFragment());
+        fragments.add(new PagerFragment());
+        fragments.add(new PagerFragment());
+    }
+
+    public void searchButton(View view){
+        startActivity(new Intent(this,SearchActivity.class));
+    }
+}
---
+++ app/src/main/java/com/example/shinelon/yoursister/yoursister/SearchActivity.java
@@ -0,0 +1,19 @@
+package com.example.shinelon.yoursister.yoursister;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.qf.administrator.yoursister.R;
+
+/**
+ * 这里是首页tablayout右边搜索按钮进来之后出现的界面
+ *
+ */
+public class SearchActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
