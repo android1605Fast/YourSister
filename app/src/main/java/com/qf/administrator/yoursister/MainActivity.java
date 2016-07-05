168ce5e701f6e6f853d777afe53d23056ad46bdd
tiancaijyy <68704811@qq.com>
2016-07-05 17:06:18 +0800

    首页基本框架搭建

--- app/src/main/java/com/qf/administrator/yoursister/MainActivity.java
+++ app/src/main/java/com/qf/administrator/yoursister/MainActivity.java
@@ -2,15 +2,22 @@ package com.qf.administrator.yoursister;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.qf.administrator.yoursister.fragment.PagerFragment;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import Adapter.ViewPagerAdapter;
+
 public class MainActivity extends AppCompatActivity{
 
-    private List<String> titleList;
     private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private List<PagerFragment> fragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -18,23 +25,30 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         initData();
         initView();
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), fragments);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     private void initView(){
         tabLayout = (TabLayout) findViewById(R.id.tablayout);
-        
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
     }
 
     private void initData(){
-        titleList = new ArrayList<>();
-        titleList.add("首页");
-        titleList.add("车评");
-        titleList.add("视频");
-        titleList.add("长策");
-        titleList.add("策划");
-        titleList.add("观点");
-        titleList.add("图片");
-        titleList.add("资讯");
-        titleList.add("经典");
+        fragments = new ArrayList<>();
+        fragments.add(new PagerFragment());
+        fragments.add(new PagerFragment());
+        fragments.add(new PagerFragment());
+        fragments.add(new PagerFragment());
+        fragments.add(new PagerFragment());
+        fragments.add(new PagerFragment());
+        fragments.add(new PagerFragment());
+        fragments.add(new PagerFragment());
+        fragments.add(new PagerFragment());
+    }
+
+    public void searchButton(View view){
+
     }
 }
